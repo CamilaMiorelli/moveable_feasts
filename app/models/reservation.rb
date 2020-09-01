5863ba24f06ef1fd3ced90ccda3e2965d7e553e0
david lusztig <davidlusztig@outlook.com>
2020-09-01 12:54:25 +0200

    end erased

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :feast
   belongs_to :user
-<<<<<<< HEAD
   validates :status, presence: true
-
 end
-=======
-  validates :status, length: { minimum: 1 }
-
-end
->>>>>>> master
