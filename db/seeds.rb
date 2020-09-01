762fbb64f37a546602699cc2fc0f1617169d4396
Camila Miorelli <camimiorelli@gmail.com>
2020-09-01 12:27:32 +0200

    Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,28 @@ puts 'Creating feasts...'
 puts 'Creating a fake users...'
 user = User.create(first_name: "Camila", last_name: "M", email: "a@a.com", password: 123456)
 
+5.times do
 indian = Feast.new
 (title: "Traditional Indian Cuisine", description: "I would like to invite everyone to my traditional indian feast.
 I am from the punjabi region of India and I would like to make a curry and have some friends over. The meal will be vegetarian friendly
-and also very spicy! Hope to meet you all soon.", meal_type: "Vegetarian", guest_limit: 5, price: 14, status: true, address: "Reguilersgracht 17, Amsterdam")
+and also very spicy! Hope to meet you all soon.", meal_type: "Vegetarian", guest_limit: 5, price: 14, available: true, address: "Reguilersgracht 17, Amsterdam")
 indian.save!
 
 cuban = Feast.new
-(title: "Camila's Lechon", description: "I will be making lechon asado for new friends!", meal_type: "Meat dish", guest_limit: 8, price: 20, status: true, address: "Gerard Doustraat 22, Amsterdam")
+(title: "Camila's Lechon", description: "I will be making lechon asado for new friends!", meal_type: "Meat dish", guest_limit: 8, price: 20, available: true, address: "Gerard Doustraat 22, Amsterdam")
 cuban.save!
 
 casual = Feast.new
-(title: "Casual Friday Tapas", description: "Hey everyone, I'm not so great at cooking, but I would love to host some tapas at my house, i'll order a mixture of vegetarian and meat dishes,", meal_type: "Ominivore's Delight", guest_limit: 15, price: 10, status: true, address: "Ijsbaanpad 8, Amsterdam")
+(title: "Casual Friday Tapas", description: "Hey everyone, I'm not so great at cooking, but I would love to host some tapas at my house, i'll order a mixture of vegetarian and meat dishes,", meal_type: "Ominivore's Delight", guest_limit: 15, price: 10, available: true, address: "Ijsbaanpad 8, Amsterdam")
 casual.save!
 
 picnic = Feast.new
-(title: "Sunday Picnic at Vondel", description: "Looking to connect with people interested in design and vegan food, I would like to host a picnic in the park, everyone can bring some vegan snacks.", meal_type: "vegan", guest_limit: 25, price: 0, status: true, address: "Vondel Park, Amsterdam")
+(title: "Sunday Picnic at Vondel", description: "Looking to connect with people interested in design and vegan food, I would like to host a picnic in the park, everyone can bring some vegan snacks.", meal_type: "vegan", guest_limit: 25, price: 0, available: true, address: "Vondel Park, Amsterdam")
 picnic.save!
 
 french = Feast.new
-(title: "Thursday Night Patisserie", description: "I am an amateur pastry chef, I love to practice my skills with small cakes and madelienes", meal_type: "Dessert", guest_limit: 3, price: 10, status: true, address: "Prinsengracht 10, Amsterdam")
+(title: "Thursday Night Patisserie", description: "I am an amateur pastry chef, I love to practice my skills with small cakes and madelienes", meal_type: "Dessert", guest_limit: 3, price: 10, available: true, address: "Prinsengracht 10, Amsterdam")
 french.save!
+end
 
 puts 'Finished!'
