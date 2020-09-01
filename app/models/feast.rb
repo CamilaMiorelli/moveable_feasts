02d74a1a5055f8237ed2aabeb4496d2d9d2e5571
david lusztig <davidlusztig@outlook.com>
2020-09-01 11:58:35 +0200

    validated feast model and changed the status to available. did a successful migration that is correct in the schema

--- app/models/feast.rb
+++ app/models/feast.rb
@@ -2,4 +2,11 @@ class Feast < ApplicationRecord
   has_many :reservations
   has_one :chatroom
   belongs_to :user
+  validates :title, length: { minimum: 10 }
+  validates :description, length: { minimum: 10 }
+  validates :meal_type, length: { minimum: 3 }
+  validates :guest_limit, length: { minimum: 1 }
+  validates :address, presence: true
+  validates :start_at, presence: true
+  validates :end_at, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_144004) do
+ActiveRecord::Schema.define(version: 2020_09_01_095107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_144004) do
     t.string "meal_type"
     t.integer "guest_limit"
     t.float "price"
-    t.boolean "status"
+    t.boolean "available"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
