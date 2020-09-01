851f716f703cca645e86f5bcd7bf363b8c9f72f7
Hans Preeker <hans_preeker@hotmail.com>
2020-09-01 14:09:07 +0200

    create home view

--- app/controllers/feasts_controller.rb
+++ app/controllers/feasts_controller.rb
@@ -1,6 +1,13 @@
 class FeastsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :index, :show ]
 
   def home
   end
+
+  def index
+  end
+
+  def show
+  end
+
 end
