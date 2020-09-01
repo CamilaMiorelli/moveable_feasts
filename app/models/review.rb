d38554bceb7d0819ad4c150e15a2ec6ba89f0288
david lusztig <davidlusztig@outlook.com>
2020-09-01 17:07:03 +0200

    added review model validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  validates :status, presence: true
-
+  validates :review_title, presence: true
+  validates :rating, inclusion: 1..10
+  validates :review_description, presence: true
 end
