d77c8da8f652978aa3bd189a9c08cbc7fc81eb64
david lusztig <davidlusztig@outlook.com>
2020-09-01 16:03:04 +0200

    added index, crete, home and show with strong params private method to feasts controller

--- app/controllers/feasts_controller.rb
+++ app/controllers/feasts_controller.rb
@@ -12,8 +12,17 @@ class FeastsController < ApplicationController
   end
 
   def index
+    @feasts = Feast.all
   end
 
   def create
+    @feast = Feast.new(strong_feasts_params)
+  end
+
+  private
+  
+  def strong_feasts_params
+    params.require(:feast).permit(:title, :description, :meal_type, :guest_limit, :price, :address, :start_at)
+
   end
 end
