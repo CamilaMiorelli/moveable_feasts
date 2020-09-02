204758a027b21188ea51105cba5121deb8d2e4e7
david lusztig <davidlusztig@outlook.com>
2020-09-02 13:11:17 +0200

    feast specific chatroom works. need to add ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,14 @@
 class MessagesController < ApplicationController
     def create
-        @chatroom = Chatroom.find(params[:chatroom_id])
-        @message = Message.new(message_params)
+        @chatroom = Chatroom.find_by(feast_id: params[:feast_id])
+        @message = Message.new(strong_messages_params)
         @message.chatroom = @chatroom
         @message.user = current_user
         if @message.save
+            ChatroomChannel.broadcast_to(
+                @chatroom,
+                render_to_string(partial: "message", locals: { message: @message })
+            )
           redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
         else
           render "chatrooms/show"
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'feasts#home'
   resources :feasts do
     resources :reservations, only: [ :new, :create, :show, :update]
-    resources :chatroom, only: [ :show ] do
-      resources :messages, only: :create
-    end
+    resource :chatroom, only: :show  do
+    resources :messages, only: :create
+  end
   end
 end
