0993c0b5e0196eb75cd1eb005e6a78914ce71a77
Camila Miorelli <camimiorelli@gmail.com>
2020-09-04 11:05:18 +0200

    Added mapbox popup and markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,6 +12,8 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
