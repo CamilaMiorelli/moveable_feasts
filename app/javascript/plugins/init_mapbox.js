0669a0daf440e7bcdc4001b6b7f1b74f81969ef3
david lusztig <davidlusztig@outlook.com>
2020-09-08 10:15:07 +0200

    mapboxv4

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,7 +32,6 @@ const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-  console.log("whatever")
 };
 
 
