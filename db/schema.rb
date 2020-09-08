d8d01ed5f238092c5bc2e4783024aa57dbdf2fde
Hans Preeker <hans_preeker@hotmail.com>
2020-09-08 15:00:50 +0200

    added avatar to index cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_134727) do
+ActiveRecord::Schema.define(version: 2020_09_08_100430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_134727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "reviewer_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
